4d6acb7c417b176e7a3c9744d487066d490bd17b
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-03-22 10:54:51 +0900

    USERモデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+ 
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+       
+  with_options presence: true do
+    validates :nickname
+    validates :password_confirmation
+    validates :birthday
+  end
+       
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
+  validates_format_of :first_name, presence: true, with: NAME_REGEX, message: '全角（漢字・ひらがな・カタカナ）を使用してください'
+  validates_format_of :last_name, presence: true, with: NAME_REGEX, message: '全角（漢字・ひらがな・カタカナ）を使用してください'
+       
+  NAME_KANA_REGEX = /\A[ァ-ヶ一]+\z/.freeze
+  validates_format_of :first_kana, presence: true, with: NAME_KANA_REGEX, message: 'に全角（カタカナ）を使用してください'
+  validates_format_of :last_kana, presence: true, with: NAME_KANA_REGEX, message: 'に全角（カタカナ）を使用してください'
+  
 end
