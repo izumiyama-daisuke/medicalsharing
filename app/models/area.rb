f5e442b5c55f89af47442a2e7dab7812f14f592a
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-03-25 13:33:21 +0900

    ActiveHash(area,prefecture)を作成

---
+++ app/models/area.rb
@@ -0,0 +1,8 @@
+class Area < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '教育者側が用意' },
+    { id: 3, name: '学びたい側が用意' },
+    { id: 4, name: '今は未定' },
+  ]
+  end
