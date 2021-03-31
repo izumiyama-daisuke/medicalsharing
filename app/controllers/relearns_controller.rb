80e93e2e76f23999b7e5966d882f068b176ca2f4
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-03-31 20:28:05 +0900

    relearnコントローラーにcreateを記述

--- app/controllers/relearns_controller.rb
+++ app/controllers/relearns_controller.rb
@@ -1,9 +1,23 @@
 class RelearnsController < ApplicationController
   def new
-    @relearn = Relearn.find(params[:id])
+    @relearn = Relearn.new
   end
+
 
   def create
+    @relearn = Relearn.create(relearn_params)
+binding.pry
+    if @relearn.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def relearn_params
+    params.require(:relearn).permit(:job_id, :gender_id, :restudy, :restudytitle, :prefecture_id, :area_id, :area_provided,
+                                  :learn_num_id, :condition).merge(user_id: current_user.id, learn_id: params[:learn_id])
+  end
 end
