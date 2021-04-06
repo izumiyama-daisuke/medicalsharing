f8a0d787340ce51c3d75cfa34e01651c7bc549bb
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-04-06 14:31:43 +0900

    チャット（サイドバー）に投稿（略を表示）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,12 @@
 class MessagesController < ApplicationController
   def index
+    @learn = Learn.find(params[:learn_id])
+    @relearns = Relearn.all.includes(:user).order("created_at DESC")
+    @learns = Learn.all.includes(:user).order("created_at DESC")
+  end
+
+  def show
+    
   end
 end
+
