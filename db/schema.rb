1c7f0bbaf13843fee035e6bf2d415286867fd208
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-04-07 13:39:36 +0900

    roomテーブルの作成（migrate）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_041836) do
+ActiveRecord::Schema.define(version: 2021_04_07_043445) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,12 @@ ActiveRecord::Schema.define(version: 2021_03_31_041836) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["learn_id"], name: "index_relearns_on_learn_id"
     t.index ["user_id"], name: "index_relearns_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
