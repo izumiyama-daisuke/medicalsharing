32df91a0ae89e4c164c121e13cab92bcb419fc93
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-04-14 19:14:40 +0900

    登録したメッセージを表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,18 @@ class MessagesController < ApplicationController
     @learn = Learn.find(params[:learn_id])
     @relearns = Relearn.all.includes(:user).order("created_at DESC")
     @learns = Learn.all.includes(:user).order("created_at DESC")
-
-    ####roomコントローラーにも同じコードを記載しています  不要か？
-    #@message = Message.new     ####roomコントローラーにも同じコードを記載しています  不要か？
-    #@room = Room.new
     @message = Message.new
     @room = Room.new
+
+
+    if params[:room_id].to_i != 0 # チャット相手を洗濯しないと入力欄を非表示 
+      room_name = Room.find(params[:room_id])##ルーム名を表示する
+      @room_name = room_name.name##ルーム名を表示する
+      @@room = Room.find(params[:room_id])##メッセージを表示する
+      @messages = @@room.messages.includes(:user).order("created_at DESC")##メッセージを表示する
+    else
+      @room_name = "ルームを選択してください！"##ルーム名を表示する
+    end
   end
 
   def create
@@ -19,6 +25,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to learn_room_messages_path(@learn, @room)
     else
+      @messages = @room.messages.includes(:user)###必要か？メッセージを表示
       render :index
     end
   end
