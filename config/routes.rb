4006aa8e490085b5eef4043f2024fe2ab0511665
izumiyama-daisuke <74485639+izumiyama-daisuke@users.noreply.github.com>
2021-04-18 14:04:35 +0900

    検索フォームを表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,21 @@ Rails.application.routes.draw do
   get 'learns/index'
   root to: "learns#index"
 
+#  resources :learns do
+#    resources :relearns, only: [:new, :create, :show]
+#    resources :rooms, only: [:new, :create, :destroy] do
+#      resources :messages, only: [:index, :create]
+#    end
+#  end
+  
   resources :learns do
     resources :relearns, only: [:new, :create, :show]
+    collection do    #追加
+      get 'search'   #
+    end              #
     resources :rooms, only: [:new, :create, :destroy] do
       resources :messages, only: [:index, :create]
     end
   end
-  
 
 end
